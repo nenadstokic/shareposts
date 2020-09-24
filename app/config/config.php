190b10b67c30c7342cc47245d0c1c397652ba495
Nenad Stokic <nstokic@gmail.com>
2020-09-24 23:24:53 +0200

    app part one done

---
+++ app/config/config.php
@@ -0,0 +1,24 @@
+<?php 
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'shareposts');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/shareposts');
+
+// Site Name
+define('SITENAME', 'SharePosts');
+
+// App version
+define('APPVERSION', '1.0.0');
+
+
+
+// FOR OTHER SITES .HTACCESS IN PUBLIC FOLDER SHOULD ALSO BE CHANGED
+// RewriteBase /traversymvc/public
---
+++ app/libraries/Core.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * App core Class
+ * Creates URL and loads core controller
+ * URL format - /controllers/method/params
+*/
+
+class Core {
+  protected $currentController = 'Pages'; // default controller to load
+  protected $currentMethod = 'index';
+  protected $params = [];
+
+  public function __construct() {
+
+    $url = $this->getUrl();
+    // look in controllers for first value
+    if(isset($url[0]) && file_exists('../app/controllers/'. ucwords($url[0]) . '.php')) {
+      // if exists, set as controller
+      $this->currentController = ucwords($url[0]);
+      // Unset 0 index
+      unset($url[0]);
+    }
+
+    //Require the controller
+    require_once '../app/controllers/' . $this->currentController . '.php';
+
+    // Instantiate controller class
+    $this->currentController = new $this->currentController;
+
+    //check for secand part of url
+    if(isset($url[1])) {
+        // check to see if method exists in controller
+        if(method_exists($this->currentController, $url[1])){
+          $this->currentMethod = $url[1];
+        }
+        // unset 1 index
+        unset($url[1]);
+    }
+
+    // Get params
+    $this->params = $url ? array_values($url) : [];
+
+    // Call a callback with array of params
+    call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+
+    //echo $this->currentMethod;
+  }
+
+  public function getUrl(){
+    if(isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
+
+  }
+}
