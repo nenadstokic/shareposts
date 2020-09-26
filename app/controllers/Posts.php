26be47b587eb72a4586b548d61713bb563cf06b7
Nenad Stokic <nstokic@gmail.com>
2020-09-26 14:44:43 +0200

    dalje

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,6 +1,12 @@
 <?php
-  class Posts {
+  class Posts extends Controller {
     public function __construct(){
       echo('Posts loaded');
+    }
+
+    public function index() {
+      $data = [];
+
+      $this->view('posts/index', $data);
     }
   }
